94cfd0efc7902bf64074464b3ff7e87b925f7f9f
ozgunasli <ozgunn.asli@gmail.com>
2019-07-16 16:58:37 +0300

    Edited

--- main/views.py
+++ main/views.py
@@ -7,16 +7,24 @@ base_url="https://api.github.com/users/"
 
 def index_view(request):
 
+    form=AramaForm()
+
     if request.method == "POST":
+
         form = AramaForm(request.POST)
+
         if form.is_valid():
-            githubname=form.cleaned_data['githubname']
+
+            githubname = form.cleaned_data.get('githubname')
 
             response=requests.get(base_url+githubname)
+
             user_info=response.json()
+
             profile = user_info
-            return render(request, 'index.html',profile)
+
+            return render(request, 'index.html',{'profile':profile})
 
     else:
 
-        return render(request,'index.html')
+        return render(request,'index.html',{'form':form})
